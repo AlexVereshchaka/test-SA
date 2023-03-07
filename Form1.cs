d1fa89778c05d5e41ad11c906bd842b34c9aff9e
Alex <86826701+AlexVereshchaka@users.noreply.github.com>
2023-03-08 00:50:40 +0200

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -22,32 +22,33 @@ namespace LB1
 			b = new Bitmap(Image.FromFile(textBox1.Text));
 			pictureBox1.Image = b;
 		}
-		private void button2_Click(object sender, EventArgs e)
-		{
-			double I, minI, maxI;
-			Color c;
-			double sum = 0, srI = 0;
-
-			minI = 255;
-			maxI = 0;
-			for (int x = 0 ; x < b.Width; x = x+1)
-			{
-				for (int y = 0; y < b.Height; y = y+1)
-				{
-					c = b.GetPixel(x, y);
-					I = (c.R + c.G + c.B) / 3;
-					sum = sum + I;
-					if (minI > I) minI = I;
-					if (maxI > I) maxI = I;
-				}
-			}
-		srI = sum / (b.Width * b.Height);
-		MessageBox.Show("середня яскравість" + srI + "min" + minI + "max" + maxI);
-		}
 
         private void pictureBox1_Click(object sender, EventArgs e)
 		{
 
 		}
+
+		private void button2_Click_1(object sender, EventArgs e)
+		{
+            double I, minI, maxI;
+            Color c;
+            double sum = 0, srI = 0;
+
+            minI = 255;
+            maxI = 0;
+            for (int x = 0; x < b.Width; x = x + 1)
+            {
+                for (int y = 0; y < b.Height; y = y + 1)
+                {
+                    c = b.GetPixel(x, y);
+                    I = (c.R + c.G + c.B) / 3;
+                    sum = sum + I;
+                    if (minI > I) minI = I;
+                    if (maxI > I) maxI = I;
+                }
+            }
+            srI = sum / (b.Width * b.Height);
+            MessageBox.Show("середня яскравість" + srI + "min" + minI + "max" + maxI);
+        }
 	}
 }
